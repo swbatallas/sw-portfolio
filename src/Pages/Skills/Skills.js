80dd0e90b400e0225ca96ee9ac905c92b8003bbc
Ulyssbat <stewartbatallasminda@gmail.com>
2022-10-19 09:58:35 +0200

    imagenes añadidas a carpeta public y skills añadidas mediante map

--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -1,45 +1,20 @@
 import React from 'react'
 import './Skills.css'
 import { Container, Image } from 'react-bootstrap'
-import mongo from './../../assets/mongo.png'
-import express from './../../assets/express.png'
-import react from './../../assets/react.png'
-import nodejs from '../../assets/nodejs.png'
 import { Animator, batch, MoveIn, Fade } from 'react-scroll-motion';
 
 
 function Skills() {
-    return (
-        <Container fluid className='skills py-5 d-flex flex-column align-items-center'>
-            <Animator animation={batch(MoveIn(-1000, 0), Fade())}>
-                <Container fluid className='mern'>
-                    <Image fluid src={mongo} />
-                </Container>
-            </Animator>
-            <Animator animation={batch(MoveIn(1000, 0), Fade())}>
-                <Container fluid className='mern'>
-                    <Image fluid src={express} />
-                </Container>
-            </Animator>
-            <Animator animation={batch(MoveIn(-1000, 0), Fade())}>
-                <Container fluid className='mern'>
-                    <Image fluid src={react} />
-                </Container>
-            </Animator>
-            <Animator animation={batch(MoveIn(1000, 0), Fade())}>
-                <Container fluid className='mern' >
-                    <Image fluid src={nodejs} />
-                </Container>
-            </Animator>
+    let skillsArray = ["mongo", "express", "react", "nodejs"]
+    return (<Container fluid>
+        {skillsArray.map((skill) => (
             <Animator animation={Fade()}>
-            <Container className='py-5 my-5 d-flex justify-content-center align-self-end'>
-                <h5 className='w-75'>Además de tener experiencia con HTML, CSS, JS,
-                    MySQL, bootstrap, Figma, i18n,
-                    AWS, Firebase, entre otros...
-                </h5>
+                <Container fluid className={skill}>
+                    <Image role='img' fluid src={`Images/${skill}.png`} alt={`${skill} icon`} />
                 </Container>
             </Animator>
-        </Container>
+        ))}
+    </Container>
     )
 }
 
