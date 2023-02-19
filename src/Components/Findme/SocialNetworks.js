d2b3bcf8f5c16f82eda5f75f05d116f604fca344
swbatallas <stewartbatallasminda@gmail.com>
2023-02-20 00:43:22 +0100

    Añade el boton de descargar cv y cambia la descripcion

--- src/Components/Findme/SocialNetworks.js
+++ src/Components/Findme/SocialNetworks.js
@@ -1,26 +1,35 @@
 import React from 'react'
 import './SocialNetworks.css'
-import { Container, Button, Image } from 'react-bootstrap'
+import { Container, Button, Image, OverlayTrigger, Tooltip } from 'react-bootstrap'
 
 
 export default function SocialNetworks() {
     let nameOfNetworks = [
-        { name: 'linkedin', url: 'https://linkedin.com/in/stewartbatallas/' },
-        { name: 'github', url: 'https://github.com/swbatallas' },
-        { name: 'twitter', url: 'https://twitter.com/sw_batallas' },
-        { name: 'instagram', url: 'https://instagram.com/sw_batallas/' },
-        { name: 'mail', url: 'mailto:stewartbatallasminda@gmail.com' }
+        { name: 'LinkedIn', url: 'https://linkedin.com/in/stewartbatallas/' },
+        { name: 'Github', url: 'https://github.com/swbatallas' },
+        { name: 'Twitter', url: 'https://twitter.com/sw_batallas' },
+        { name: 'Instagram', url: 'https://instagram.com/sw_batallas/' },
+        { name: 'Mail', url: 'mailto:stewartbatallasminda@gmail.com' },
+        { name: 'CV', url: '#' }
     ]
 
     return (
         <Container className='d-flex flex-row justify-content-center align-items-center my-4' >
             {nameOfNetworks.map(network => (
-                <a href={network.url} target="_blank" rel="noreferrer" key={network.name}>
-                    <Button className='buttons d-flex justify-content center align-items-center'
-                        variant='light'>
-                        <Image className='' role='img' src={`Media/${network.name}.svg`} alt={network.name} />
-                    </Button>
-                </a>
+                <OverlayTrigger delay={{ hide: 350, show: 250 }} overlay={(props) => (
+                    <Tooltip {...props}>
+                        {network.name}
+                    </Tooltip>
+                )}
+                    placement="top">
+                    <a href={network.url} target="_blank" rel="noreferrer" key={network.name}>
+                        <Button className='buttons d-flex justify-content center align-items-center'
+                            variant='light'>
+                            <Image className='' role='img' src={`Media/${network.name.toLowerCase()}.svg`} alt={network.name} />
+                        </Button>
+                    </a>
+
+                </OverlayTrigger>
             ))}
         </Container>
     )
