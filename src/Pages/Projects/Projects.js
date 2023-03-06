6f70e316ca6664220c0b66d732898aeeb5789aab
Ulyssbat <stewartbatallasminda@gmail.com>
2023-03-06 12:40:12 +0100

    Añade grid a los projects

--- src/Pages/Projects/Projects.js
+++ src/Pages/Projects/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Projects.css'
 import { projectData } from './projectData'
-import { Button, Container, Image } from 'react-bootstrap'
+import { Button, Row, Col, Container, Image } from 'react-bootstrap'
 
 
 
@@ -9,20 +9,24 @@ function Projects() {
 
   return (
     <Container className='min-vh-100' fluid>
-      <h1 className='text-center'>Proyectos</h1>
-      <Container className='mb-2 d-flex justify-content-center align-items-center' fluid>
+      <h1 className='title text-center text-uppercase mb-4'>Proyectos</h1>
+      <Row>
         {projectData.map((project, key) => (
-          <div className='project-container m-4' key={key}>
-            <Image src={project.src} alt={project.name} />
-            <Container
-              className='project-description d-flex flex-column justify-content-center align-items-center'>
-              <h2>{project.name}</h2>
-              <p>{project.description.es}</p>
-              <Button>Ver mas</Button>
-            </Container>
-          </div>
+          <Col xs={12} md={6} lg={4} xl={3}>
+            <div className='project-container m-4' key={key}>
+              <Image src={project.src} alt={project.name} />
+              <Container
+                className='project-description d-flex flex-column justify-content-center align-items-center'>
+                <h2>{project.name}</h2>
+                <p>{project.description.es}</p>
+                <Button href={project.url} target="blank">
+                  Visitar web
+                </Button>
+              </Container>
+            </div>
+          </Col>
         ))}
-      </Container>
+      </Row>
     </Container>
 
   )
--- src/Pages/Projects/projectData.js
+++ src/Pages/Projects/projectData.js
@@ -3,15 +3,18 @@ export const projectData = [
         name: "Fraggen",
         src: "Media/fraggen.jpg",
         description: {
-            "es": "Trivial desarrollado con React JS",
-            "en": "Trivial developed with React JS"
+            es: "Trivial por categorías con cuenta atrás. Incluye inicio de sesión, registro de partida y clasificación por puntos. Desarrollado con React JS y con base de datos creada en mongoDB.",
+            en: "Trivial of categories with back countdown. Includes sign in, game register and points ranking. Developed with React JS and database created on mongoDB."
         },
         url: "https://trivial-fe.herokuapp.com/"
     },
     {
         name: "Unio",
         src: "Media/unio.jpg",
-        description: "App de lectura de base de datos basada en personajes de Marvel",
+        description: {
+            es: "Basada en la API de Marvel, Unio es una enciclopedia interactiva con información sobre los personajes de comic de Marvel. Incluye buscador, carga de personajes en scroll, ranking de personajes, valoraciones, comentarios y más. Desarrollado con React JS y base de datos creada en mongoDB.",
+            en: ""
+        },
         url: "https://fe-unio.herokuapp.com/"
     }
 ]
